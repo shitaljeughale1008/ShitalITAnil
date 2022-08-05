adaefd69398368d8dcd7a6da353966ac4f1e8ae2
tganesh169 <tganesh169@gmail.com>
2022-08-05 23:23:38 +0530

    Completed

---
+++ src/main/java/com/example/Controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.Controller;
+
+public class Controller {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("completed by Ganesh   thank to me");
+		
+		
+	}
+	
+	
+}
