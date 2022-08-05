ff58f647c0a902e0dcefdeb2db4b817b30c333f2
shitaljeughale1008 <shitaljeughale1008@gmail.com>
2022-08-05 23:08:56 +0530

    project created

---
+++ src/main/java/com/example/demo/GitFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFirstProjectApplication.class, args);
+	}
+
+}
